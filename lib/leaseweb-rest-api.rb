abe2eabf4cf7d5b163c612cfa0cc19aa2dae2b44
Arnoud Vermeer <a.vermeer@tech.leaseweb.com>
2017-01-30 15:18:33 +0100

    Remove v1 prepend in preparation of new versions

--- lib/leaseweb-rest-api.rb
+++ lib/leaseweb-rest-api.rb
@@ -11,7 +11,7 @@ class LeasewebAPI
   format :json
   # debug_output $stderr
 
-  base_uri 'https://api.leaseweb.com/v1'
+  base_uri 'https://api.leaseweb.com'
 
   def apiKeyAuth(apikey)
     @options = { headers: { 'X-Lsw-Auth' => apikey } }
@@ -62,21 +62,21 @@ class LeasewebAPI
 
   # Domains
   def getDomains
-    self.class.get('/domains', @options)
+    self.class.get('/v1/domains', @options)
   end
 
   def getDomain(domain)
-    self.class.get("/domains/#{domain}", @options)
+    self.class.get("/v1/domains/#{domain}", @options)
   end
 
   def updateDomain(domain, ttl)
     opt = @options.merge!(body: { ttl: ttl })
 
-    self.class.put("/domains/#{domain}", opt)
+    self.class.put("/v1/domains/#{domain}", opt)
   end
 
   def getDNSRecords(domain)
-    self.class.get("/domains/#{domain}/dnsRecords", @options)
+    self.class.get("/v1/domains/#{domain}/dnsRecords", @options)
   end
 
   def createDNSRecords(domain, host, content, type, priority = nil)
@@ -86,11 +86,11 @@ class LeasewebAPI
       opt[:body][:priority] = priority
     end
 
-    self.class.post("/domains/#{domain}/dnsRecords", opt)
+    self.class.post("/v1/domains/#{domain}/dnsRecords", opt)
   end
 
   def getDNSRecord(domain, dnsRecordId)
-    self.class.get("/domains/#{domain}/dnsRecords/#{dnsRecordId}", @options)
+    self.class.get("/v1/domains/#{domain}/dnsRecords/#{dnsRecordId}", @options)
   end
 
   def updateDNSRecord(domain, dnsRecordId, host, content, type, priority = nil)
@@ -100,103 +100,103 @@ class LeasewebAPI
       opt[:body][:priority] = priority
     end
 
-    self.class.put("/domains/#{domain}/dnsRecords/#{dnsRecordId}", opt)
+    self.class.put("/v1/domains/#{domain}/dnsRecords/#{dnsRecordId}", opt)
   end
 
   def deleteDNSRecord(domain, dnsRecordId)
-    self.class.delete("/domains/#{domain}/dnsRecords/#{dnsRecordId}", @options)
+    self.class.delete("/v1/domains/#{domain}/dnsRecords/#{dnsRecordId}", @options)
   end
 
   # Rescue
   def getRescueImages
-    self.class.get('/rescueImages', @options)
+    self.class.get('/v1/rescueImages', @options)
   end
 
   # BareMetals
   def getBareMetals
-    self.class.get('/bareMetals', @options)
+    self.class.get('/v1/bareMetals', @options)
   end
 
   def getBareMetal(bareMetalId)
-    self.class.get("/bareMetals/#{bareMetalId}", @options)
+    self.class.get("/v1/bareMetals/#{bareMetalId}", @options)
   end
 
   def updateBareMetal(bareMetalId, reference)
     opt = @options.merge!(body: { reference: reference })
 
-    self.class.put("/bareMetals/#{bareMetalId}", opt)
+    self.class.put("/v1/bareMetals/#{bareMetalId}", opt)
   end
 
   def getSwitchPort(bareMetalId)
-    self.class.get("/bareMetals/#{bareMetalId}/switchPort", @options)
+    self.class.get("/v1/bareMetals/#{bareMetalId}/switchPort", @options)
   end
 
   def postSwitchPortOpen(bareMetalId)
-    self.class.post("/bareMetals/#{bareMetalId}/switchPort/open", @options)
+    self.class.post("/v1/bareMetals/#{bareMetalId}/switchPort/open", @options)
   end
 
   def postSwitchPortClose(bareMetalId)
-    self.class.post("/bareMetals/#{bareMetalId}/switchPort/close", @options)
+    self.class.post("/v1/bareMetals/#{bareMetalId}/switchPort/close", @options)
   end
 
   def getPowerStatus(bareMetalId)
-    self.class.get("/bareMetals/#{bareMetalId}/powerStatus", @options)
+    self.class.get("/v1/bareMetals/#{bareMetalId}/powerStatus", @options)
   end
 
   def getIPs(bareMetalId)
-    self.class.get("/bareMetals/#{bareMetalId}/ips", @options)
+    self.class.get("/v1/bareMetals/#{bareMetalId}/ips", @options)
   end
 
   def getIP(bareMetalId, ipAddress)
-    self.class.get("/bareMetals/#{bareMetalId}/ips/#{ipAddress}", @options)
+    self.class.get("/v1/bareMetals/#{bareMetalId}/ips/#{ipAddress}", @options)
   end
 
   def updateIP(bareMetalId, ipAddress, reverseLookup = '', nullRouted = 0)
     opt = @options.merge!(body: { reverseLookup: reverseLookup, nullRouted: nullRouted })
 
-    self.class.put("/bareMetals/#{bareMetalId}/ips/#{ipAddress}", opt)
+    self.class.put("/v1/bareMetals/#{bareMetalId}/ips/#{ipAddress}", opt)
   end
 
   def getIpmiCredentials(bareMetalId)
-    self.class.get("/bareMetals/#{bareMetalId}/ipmiCredentials", @options)
+    self.class.get("/v1/bareMetals/#{bareMetalId}/ipmiCredentials", @options)
   end
 
   def getNetworkUsage(bareMetalId)
-    self.class.get("/bareMetals/#{bareMetalId}/networkUsage", @options)
+    self.class.get("/v1/bareMetals/#{bareMetalId}/networkUsage", @options)
   end
 
   def getNetworkUsageBandWidth(bareMetalId, dateFrom, dateTo, format = 'json')
-    self.class.get("/bareMetals/#{bareMetalId}/networkUsage/bandWidth", formatRequest(dateFrom, dateTo, format))
+    self.class.get("/v1/bareMetals/#{bareMetalId}/networkUsage/bandWidth", formatRequest(dateFrom, dateTo, format))
   end
 
   def getNetworkUsageDataTraffic(bareMetalId, dateFrom, dateTo, format = 'json')
-    self.class.get("/bareMetals/#{bareMetalId}/networkUsage/dataTraffic", formatRequest(dateFrom, dateTo, format))
+    self.class.get("/v1/bareMetals/#{bareMetalId}/networkUsage/dataTraffic", formatRequest(dateFrom, dateTo, format))
   end
 
   def postReboot(bareMetalId)
-    self.class.post("/bareMetals/#{bareMetalId}/reboot", @options)
+    self.class.post("/v1/bareMetals/#{bareMetalId}/reboot", @options)
   end
 
   def installServer(bareMetalId, osId, hdd = [])
     opt = @options.merge!(body: { osId: osId, hdd: hdd }, query_string_normalizer: ->(h) { HashToURIConversion.new.to_params(h) })
 
-    self.class.post("/bareMetals/#{bareMetalId}/install", opt)
+    self.class.post("/v1/bareMetals/#{bareMetalId}/install", opt)
   end
 
   def postResqueMode(bareMetalId, osId)
     opt = @options.merge!(body: { osId: osId })
 
-    self.class.post("/bareMetals/#{bareMetalId}/rescueMode", opt)
+    self.class.post("/v1/bareMetals/#{bareMetalId}/rescueMode", opt)
   end
 
   def getRootPassword(bareMetalId, format = 'json')
     opt = @options.merge!(headers: formatHeader(format))
 
-    self.class.get("/bareMetals/#{bareMetalId}/rootPassword", opt)
+    self.class.get("/v1/bareMetals/#{bareMetalId}/rootPassword", opt)
   end
 
   def getInstallationStatus(bareMetalId)
-    response = self.class.get("/bareMetals/#{bareMetalId}/installationStatus", @options)
+    response = self.class.get("/v1/bareMetals/#{bareMetalId}/installationStatus", @options)
 
     if response['installationStatus'].include?('initRootPassword')
       response['installationStatus']['initRootPassword'] = decrypt(response['installationStatus']['initRootPassword'])
@@ -210,131 +210,123 @@ class LeasewebAPI
   end
 
   def getLeases(bareMetalId)
-    self.class.get("/bareMetals/#{bareMetalId}/leases", @options)
+    self.class.get("/v1/bareMetals/#{bareMetalId}/leases", @options)
   end
 
   def setLease(bareMetalId, bootFileName)
     opt = @options.merge!(body: { bootFileName: bootFileName })
 
-    self.class.post("/bareMetals/#{bareMetalId}/leases", opt)
+    self.class.post("/v1/bareMetals/#{bareMetalId}/leases", opt)
   end
 
   def getLease(bareMetalId, macAddress)
-    self.class.get("/bareMetals/#{bareMetalId}/leases/#{macAddress}", @options)
+    self.class.get("/v1/bareMetals/#{bareMetalId}/leases/#{macAddress}", @options)
   end
 
   def deleteLease(bareMetalId, macAddress)
-    self.class.delete("/bareMetals/#{bareMetalId}/leases/#{macAddress}", @options)
-  end
-
-  # New install call
-  def install(bareMetalId, operatingSystemId, options = {})
-    options[:operatingSystemId] = operatingSystemId
-    opt = @options.merge!(body: options.to_json)
-
-    self.class.post("/bmpapi/bareMetals/#{bareMetalId}/install", opt)
+    self.class.delete("/v1/bareMetals/#{bareMetalId}/leases/#{macAddress}", @options)
   end
 
   # Private Networks
   def getPrivateNetworks
-    self.class.get('/privateNetworks', @options)
+    self.class.get('/v1/privateNetworks', @options)
   end
 
   # TODO: check post with name
   def createPrivateNetworks(name = '')
     opt = @options.merge!(body: { name: name })
 
-    self.class.post('/privateNetworks', opt)
+    self.class.post('/v1/privateNetworks', opt)
   end
 
   def getPrivateNetwork(id)
-    self.class.get("/privateNetworks/#{id}", @options)
+    self.class.get("/v1/privateNetworks/#{id}", @options)
   end
 
   # TODO: Check with Jeroen if it works
   def updatePrivateNetwork(id, name = '')
     opt = @options.merge!(body: { name: name })
 
-    self.class.put("/privateNetworks/#{id}", opt)
+    self.class.put("/v1/privateNetworks/#{id}", opt)
   end
 
   def deletePrivateNetwork(id)
-    self.class.delete("/privateNetworks/#{id}", @options)
+    self.class.delete("/v1/privateNetworks/#{id}", @options)
   end
 
   def createPrivateNetworksBareMetals(id, bareMetalId)
     opt = @options.merge!(body: { bareMetalId: bareMetalId })
 
-    self.class.post("/privateNetworks/#{id}/bareMetals", opt)
+    self.class.post("/v1/privateNetworks/#{id}/bareMetals", opt)
   end
 
   def deletePrivateNetworksBareMetals(id, bareMetalId)
-    self.class.delete("/privateNetworks/#{id}/bareMetals/#{bareMetalId}", @options)
+    self.class.delete("/v1/privateNetworks/#{id}/bareMetals/#{bareMetalId}", @options)
   end
 
   # Operating Systems
   def getOperatingSystems
-    self.class.get('/operatingSystems', @options)
+    self.class.get('/v1/operatingSystems', @options)
   end
 
   def getOperatingSystem(operatingSystemId)
-    self.class.get("/operatingSystems/#{operatingSystemId}", @options)
+    self.class.get("/v1/operatingSystems/#{operatingSystemId}", @options)
   end
 
   def getControlPanels(operatingSystemId)
-    self.class.get("/operatingSystems/#{operatingSystemId}/controlPanels", @options)
+    self.class.get("/v1/operatingSystems/#{operatingSystemId}/controlPanels", @options)
   end
 
   def getControlPanel(operatingSystemId, controlPanelId)
-    self.class.get("/operatingSystems/#{operatingSystemId}/controlPanels/#{controlPanelId}", @options)
+    self.class.get("/v1/operatingSystems/#{operatingSystemId}/controlPanels/#{controlPanelId}", @options)
   end
 
   def getPartitionSchema(operatingSystemId, bareMetalId)
     opt = @options.merge!(query: { serverPackId: bareMetalId })
 
-    self.class.get("/operatingSystems/#{operatingSystemId}/partitionSchema", opt)
+    self.class.get("/v1/operatingSystems/#{operatingSystemId}/partitionSchema", opt)
   end
 
   # IPs
   def getIps
-    self.class.get('/ips', @options)
+    self.class.get('/v1/ips', @options)
   end
 
   def getIp(ipAddress)
-    self.class.get("/ips/#{ipAddress}", @options)
+    self.class.get("/v1/ips/#{ipAddress}", @options)
   end
 
   def updateIp(ipAddress, reverseLookup = '', nullRouted = 0)
     opt = @options.merge!(body: { reverseLookup: reverseLookup, nullRouted: nullRouted })
 
-    self.class.put("/ips/#{ipAddress}", opt)
+    self.class.put("/v1/ips/#{ipAddress}", opt)
   end
 
   # Pay as you go
   def getPAYGInstances
-    self.class.get('/payAsYouGo/bareMetals/instances', @options)
+    self.class.get('/v1/payAsYouGo/bareMetals/instances', @options)
   end
 
   def createPAYGInstance(modelId)
     opt = @options.merge!(model: modelId)
 
-    self.class.post('/payAsYouGo/bareMetals/instances', opt)
+    self.class.post('/v1/payAsYouGo/bareMetals/instances', opt)
   end
 
   def getPAYGInstance(bareMetalId)
-    self.class.get("/payAsYouGo/bareMetals/instances/#{bareMetalId}", @options)
+    self.class.get("/v1/payAsYouGo/bareMetals/instances/#{bareMetalId}", @options)
   end
 
   def destroyPAYGInstance(bareMetalId)
-    self.class.post("/payAsYouGo/bareMetals/instances/#{bareMetalId}/destroy", @options)
+    self.class.post("/v1/payAsYouGo/bareMetals/instances/#{bareMetalId}/destroy", @options)
   end
 
   def getPAYGModels
-    self.class.get('/payAsYouGo/bareMetals/models', @options)
+    self.class.get('/v1/payAsYouGo/bareMetals/models', @options)
   end
 
   def getPAYGModelInstance(modelId)
-    self.class.get("/payAsYouGo/bareMetals/models/#{modelId}", @options)
+    self.class.get("/v1/payAsYouGo/bareMetals/models/#{modelId}", @options)
   end
 
   protected
